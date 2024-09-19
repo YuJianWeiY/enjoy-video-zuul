62dcf310497b37909625bb86c2a1809ca0ce9d3e
YuJianWeiY <119394847+YuJianWeiY@users.noreply.github.com>
2024-09-19 19:28:01 +0800

    enjoy-video-zuul
    enjoy-video-zuul

--- src/main/java/com/enjoy/video/EnjoyVideoZuulApp.java
+++ src/main/java/com/enjoy/video/EnjoyVideoZuulApp.java
@@ -1,13 +1,12 @@
 package com.enjoy.video;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-@EnableZuulProxy //开启Zuul网关功能
+@EnableZuulProxy
 public class EnjoyVideoZuulApp {
 
     public static void main(String[] args){
